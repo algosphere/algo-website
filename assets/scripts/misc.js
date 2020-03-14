92899904f5d5211e49698551a9731b41fe5cfd6d
Victor Massé <victor.dhamma@gmail.com>
2020-03-14 12:59:55 +0100

    update algonews layout

--- assets/scripts/misc.js
+++ assets/scripts/misc.js
@@ -24,6 +24,22 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+// LINK TO TABS
+$(document).ready(() => {
+  var url = window.location.href;
+  if (url.indexOf("#") > 0){
+  var activeTab = url.substring(url.indexOf("#") + 1);
+    $('.nav[role="tablist"] a[href="#'+activeTab+'"]').tab('show');
+  }
+
+  $('a[role="tab"]').on("click", function() {
+    var newUrl;
+    const hash = $(this).attr("href");
+      newUrl = url.split("#")[0] + hash;
+    history.replaceState(null, null, newUrl);
+  });
+});
+
 // EXTERNAL LINKS
 var links = $(document.links).filter(function() {
   return this.hostname !== location.hostname
