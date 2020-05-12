30a21a4c169089d852888ae52a752246d714a449
Victor Massé <victor.dhamma@gmail.com>
2020-05-12 14:43:29 +0200

    debug cookies

--- gulpfile.js
+++ gulpfile.js
@@ -26,26 +26,13 @@ function htmlDev() {
 
 function htmlProd() {
   return src('public/**/*.html')
-    .pipe(replace('&laquo; ', '&laquo;&#160;'))
-    .pipe(replace(' &raquo;', '&#160;&raquo;'))
-    .pipe(replace('« ', '&laquo;&#160;'))
-    .pipe(replace(' »', '&#160;&raquo;'))
-    .pipe(replace(' :', '&#160;:'))
-    .pipe(replace('&#160;:root', ' :root')) // fix overiding style
-    .pipe(replace(' ;', '&#160;;'))
-    .pipe(replace(' !', '&#160;!'))
-    .pipe(replace(' ?', '&#160;?'))
-    .pipe(replace(' %', '&#160;%'))
-    .pipe(replace(' €', '&#160;€'))
+
     .pipe(dest('public'))
 }
 
 function imgGlobal() {
   return src('public/img/**/*.*')
-    .pipe(imagemin([
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 3}),
-    ]))
+
     .pipe(dest('public/img'))
 }
 
