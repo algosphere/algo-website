019c68898f540e84918ca93d241449b8d9eb0251
Victor Massé <victor.dhamma@gmail.com>
2020-06-03 10:58:20 +0200

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const { src, dest, series } = require('gulp'),
       replace               = require('gulp-replace'),
-      beautify              = require('gulp-pretty-html'),
       imagemin              = require('gulp-imagemin');
 
 function htmlDev() {
@@ -16,11 +15,6 @@ function htmlDev() {
     .pipe(replace(' ?', '&#160;?'))
     .pipe(replace(' %', '&#160;%'))
     .pipe(replace(' €', '&#160;€'))
-    .pipe(beautify({
-      indent_size: 2,
-      preserve_newlines: false,
-      extra_liners: []
-    }))
     .pipe(dest('public'))
 }
 
