affd23b2fe915a0d7e74c9f6ce5c472da69af8e9
Victor Massé <victor.dhamma@gmail.com>
2020-06-03 11:15:40 +0200

    update scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest, series } = require('gulp'),
       replace               = require('gulp-replace'),
       imagemin              = require('gulp-imagemin');
 
-function htmlDev() {
+function html() {
   return src('public/**/*.html')
     .pipe(replace('&laquo; ', '&laquo;&#160;'))
     .pipe(replace(' &raquo;', '&#160;&raquo;'))
@@ -18,23 +18,7 @@ function htmlDev() {
     .pipe(dest('public'))
 }
 
-function htmlProd() {
-  return src('public/**/*.html')
-    .pipe(replace('&laquo; ', '&laquo;&#160;'))
-    .pipe(replace(' &raquo;', '&#160;&raquo;'))
-    .pipe(replace('« ', '&laquo;&#160;'))
-    .pipe(replace(' »', '&#160;&raquo;'))
-    .pipe(replace(' :', '&#160;:'))
-    .pipe(replace('&#160;:root', ' :root')) // fix overiding style
-    .pipe(replace(' ;', '&#160;;'))
-    .pipe(replace(' !', '&#160;!'))
-    .pipe(replace(' ?', '&#160;?'))
-    .pipe(replace(' %', '&#160;%'))
-    .pipe(replace(' €', '&#160;€'))
-    .pipe(dest('public'))
-}
-
-function imgGlobal() {
+function images() {
   return src('public/img/**/*.*')
     .pipe(imagemin([
       imagemin.jpegtran({progressive: true}),
@@ -43,5 +27,4 @@ function imgGlobal() {
     .pipe(dest('public/img'))
 }
 
-exports.dev = series(htmlDev, imgGlobal);
-exports.prod = series(htmlProd, imgGlobal);
+exports.default = series(html, images);
