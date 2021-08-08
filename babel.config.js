cc34c51aa06a9e0aecc8c45bf2c63f6af5397d5f
Victor Massé <victor.dhamma@gmail.com>
2021-08-08 16:38:06 +0200

    remove babel config

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [
-    ["@babel/preset-env", {
-      targets: "> 0.25%, not dead",
-      useBuiltIns: "usage",
-      corejs: require("./package.json").dependencies["core-js"]
-    }]
-  ]
-};
